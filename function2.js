7c75b1ec5a60df3b51deb70fa85ebed2aafb4fff
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-22 12:34:05 +0000

    funtion2.js

---
+++ function2.js
@@ -0,0 +1,8 @@
+//  qui prend deux nombres en paramètres et retourne leur produit.
+//  Utilisez cette fonction pour afficher le produit de deux nombres.
+function membre (x, y ){
+    let somme=x * y;
+    return somme
+}
+console.log(membre(10, 15))
+
