9914cbbd2375b096f455f4a8f26dcd3885062294
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-22 14:40:36 +0000

    funtion3.js

---
+++ function3.js
@@ -0,0 +1,13 @@
+//  qui prend deux nombres en paramètres et retourne le plus grand des deux. 
+// Utilisez cette fonction pour afficher le maximum entre deux nombres.
+function max (ikram, meryem){
+    if (ikram > meryem){
+         return ikram
+    }
+       else {
+        return meryem
+       }
+}
+const resultat = max (15, 20);
+console.log(resultat);
+
