8ee172097a00df8ad1cbd85618389f53228c3b46
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-22 12:25:46 +0000

    funtion1.js

---
+++ function1.js
@@ -0,0 +1,7 @@
+//  qui prend deux nombres en paramètres et retourne leur somme.
+//  Utilisez cette fonction pour afficher la somme de deux nombres.
+function membre (x, y ){
+    let somme=x+y;
+    return somme
+}
+console.log(membre(1,1))
