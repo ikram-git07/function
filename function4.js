a3de50f4525497e063aab871004467d83a61cba2
ikram-git07 <arbaouiikram326@gmail.com>
2026-09-22 14:48:46 +0000

    funtion4.js

---
+++ function4.js
@@ -0,0 +1,10 @@
+function min (mohammed, fadou){
+    if (mohammed < fadou ){
+        return mohammed 
+    } 
+    else {
+        return fadou 
+    }
+}
+const resultat = min(11, 10);
+console.log(resultat);
